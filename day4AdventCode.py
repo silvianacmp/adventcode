5d0e40c2c55b84d5844dde6c4f4394e1916bae88
silviana campian <silviana_cimpian@yahoo.com>
2015-12-06 00:39:55 +0200

    Day 4 for advent code

--- day4AdventCode.py
+++ day4AdventCode.py
@@ -6,6 +6,8 @@ while True:
     h.update("iwrupvqb" + str(number))
     hashed = h.hexdigest()
     print hashed
+    # for part 1, with 5 zeroes, must change index to 5
+    # and string to contain 5 zeroes
     if hashed[0:6] == "000000":
         break
     number += 1
